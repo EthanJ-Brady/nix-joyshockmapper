521dbb3128aae08527774939b716e9d501f29333
EthanJ-Brady <git@ethanbrady.xyz>
2025-01-25 21:23:55 -0700

    TEMP

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "A flake for obtaining joy shock mapper on nixos";
+  description = "A flake for obtaining JoyShockMapper on NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
-    {
-      nixpkgs,
-      ...
-    }:
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -18,12 +15,14 @@
       packages.${system}.joyshockmapper = pkgs.stdenv.mkDerivation {
         pname = "JoyShockMapper";
         version = "3.6.0";
+
         src = pkgs.fetchFromGitHub {
           owner = "Electronicks";
           repo = "JoyShockMapper";
           rev = "e0bfca8e590c02389f5660d81ae9e4d268acc19b";
           sha256 = "sha256-HFTiFCGnQ8B4lVYLaqpo37L87igJb7iZSdf1bUpAEbo=";
         };
+
         buildInputs = with pkgs; [
           clang
           gtk3
@@ -33,17 +32,13 @@
           SDL2
           hidapi
         ];
+
         nativeBuildInputs = with pkgs; [
           cmake
           pkg-config
           git
         ];
-        buildPhase = ''
-          mkdir build
-          cd build
-          cmake .. -DCMAKE_CXX_COMPILER=clang++
-          cmake --build .
-        '';
+
         installPhase = ''
           mkdir -p $out/bin
           cp build/JoyShockMapper $out/bin

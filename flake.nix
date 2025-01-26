402afa8d9994104ee41525aae72f3d8cabf36c5b
EthanJ-Brady <git@ethanbrady.xyz>
2025-01-25 21:21:03 -0700

    Setup a basic flake for installing joyshockmapper that currently fails

--- flake.nix
+++ flake.nix
@@ -6,12 +6,48 @@
   };
 
   outputs =
-    { self, nixpkgs }:
     {
-
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.joyshockmapper = pkgs.stdenv.mkDerivation {
+        pname = "JoyShockMapper";
+        version = "3.6.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "Electronicks";
+          repo = "JoyShockMapper";
+          rev = "e0bfca8e590c02389f5660d81ae9e4d268acc19b";
+          sha256 = "sha256-HFTiFCGnQ8B4lVYLaqpo37L87igJb7iZSdf1bUpAEbo=";
+        };
+        buildInputs = with pkgs; [
+          clang
+          gtk3
+          libappindicator-gtk3
+          libevdev
+          libusb1
+          SDL2
+          hidapi
+        ];
+        nativeBuildInputs = with pkgs; [
+          cmake
+          pkg-config
+          git
+        ];
+        buildPhase = ''
+          mkdir build
+          cd build
+          cmake .. -DCMAKE_CXX_COMPILER=clang++
+          cmake --build .
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/JoyShockMapper $out/bin
+        '';
+      };
     };
 }

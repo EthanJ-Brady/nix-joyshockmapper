861dfaa822a7bc6bee3d8d6202e6942da24ddc06
EthanJ-Brady <git@ethanbrady.xyz>
2025-01-26 01:30:21 -0700

    Attempted to add safe dependencies to the cmake file

--- flake.nix
+++ flake.nix
@@ -3,10 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # Add SDL2 as a separate input
+    sdl2 = {
+      url = "github:libsdl-org/SDL/release-2.28.5";
+      flake = false;
+    };
+
+    # Add magic_enum as a new input
+    magic_enum = {
+      url = "github:Neargye/magic_enum/v0.9.5";
+      flake = false;
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      nixpkgs,
+      sdl2,
+      magic_enum,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -38,6 +55,28 @@
           pkg-config
           git
         ];
+
+        # Modify the configure phase to handle dependencies
+        configurePhase = ''
+          # Create directories for local dependencies
+          mkdir -p external/SDL2
+          mkdir -p external/magic_enum
+
+          # Copy SDL2 and magic_enum sources
+          cp -r ${sdl2}/* external/SDL2/
+          cp -r ${magic_enum}/* external/magic_enum/
+
+          # Modify CMake to use local sources
+          cmake -B build \
+            -DCPM_SDL2_SOURCE=external/SDL2 \
+            -DCPM_MAGIC_ENUM_SOURCE=external/magic_enum \
+            -DCPM_USE_LOCAL_PACKAGES=ON \
+            -DCPM_LOCAL_PACKAGES_ONLY=ON
+        '';
+
+        buildPhase = ''
+          cmake --build build
+        '';
 
         installPhase = ''
           mkdir -p $out/bin

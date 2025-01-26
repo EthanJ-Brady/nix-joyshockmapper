a55fefc754d162a3778358a5827a7c0319cfba87
EthanJ-Brady <git@ethanbrady.xyz>
2025-01-25 20:55:56 -0700

    Named and formatted the flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A very basic flake";
+  description = "A flake for obtaining joy shock mapper on nixos";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs }:
+    {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-  };
+    };
 }

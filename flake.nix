7b6905c6ee30ec229507d7a7f5a0f31efd68a562
EthanJ-Brady <git@ethanbrady.xyz>
2025-01-26 01:48:36 -0700

    Got the magic_enum dependency to work

--- flake.nix
+++ flake.nix
@@ -3,25 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    # Add SDL2 as a separate input
-    sdl2 = {
-      url = "github:libsdl-org/SDL/release-2.28.5";
-      flake = false;
-    };
-
-    # Add magic_enum as a new input
-    magic_enum = {
-      url = "github:Neargye/magic_enum/v0.9.5";
-      flake = false;
-    };
   };
 
   outputs =
     {
       nixpkgs,
-      sdl2,
-      magic_enum,
       ...
     }:
     let
@@ -56,20 +42,17 @@
           git
         ];
 
-        # Modify the configure phase to handle dependencies
         configurePhase = ''
-          # Create directories for local dependencies
           mkdir -p external/SDL2
           mkdir -p external/magic_enum
+          cp -r ${pkgs.SDL2}/* external/SDL2/
+          cp -r ${pkgs.magic-enum}/* external/magic_enum/
 
-          # Copy SDL2 and magic_enum sources
-          cp -r ${sdl2}/* external/SDL2/
-          cp -r ${magic_enum}/* external/magic_enum/
-
-          # Modify CMake to use local sources
+          # Use absolute paths and be explicit about sources
           cmake -B build \
-            -DCPM_SDL2_SOURCE=external/SDL2 \
-            -DCPM_MAGIC_ENUM_SOURCE=external/magic_enum \
+            -DCMAKE_PREFIX_PATH="$PWD/external/SDL2;$PWD/external/magic_enum" \
+            -DCPM_SDL2_SOURCE="$PWD/external/SDL2" \
+            -DCPM_MAGIC_ENUM_SOURCE="$PWD/external/magic_enum" \
             -DCPM_USE_LOCAL_PACKAGES=ON \
             -DCPM_LOCAL_PACKAGES_ONLY=ON
         '';
